5fefd47f99239384d6dc6da85b26ad03da6303e6
Juraj Vanko <jur.vanko@gmail.com>
2022-03-05 16:25:49 +0100

    config

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
     // setting host should not be necessary
     // host: '0.0.0.0:8080'7
     proxy: 'http://0.0.0.0:3000',
-    host: '0.0.0.0:3000',
-    port: 3000,
+    host: '0.0.0.0',
+    port:3000,
     public: '0.0.0.0:3000',
     disableHostCheck: true,
   },
