2a3c2d7de406ee2a0867008f411dd5ab421e9958
Juraj Vanko <jur.vanko@gmail.com>
2022-03-05 21:46:05 +0100

    update config

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,12 @@ module.exports = {
     // setting host should not be necessary
     // host: '0.0.0.0:8080'7
     
-    host: '0.0.0.0',
+    host: 'localhost',
     port:3000,
     disableHostCheck: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }
   },
 }
