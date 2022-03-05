eecf7009b7a5155f35fc22ba8ba5cbd318e346d8
Juraj Vanko <jur.vanko@gmail.com>
2022-03-05 16:16:43 +0100

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ module.exports = {
   publicPath: '/',
   devServer: {
     // setting host should not be necessary
-    // host: '0.0.0.0:8080'
+    // host: '0.0.0.0:8080'7
+    host: '0.0.0.0',
+    port: 3000,
     public: '0.0.0.0:3000',
     disableHostCheck: true,
   },
